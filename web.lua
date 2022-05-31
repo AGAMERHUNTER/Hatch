746bd82af0a8e72cbc0783c30fd9a7270b8119d2
hosadhej31 <105975897+hosadhej31@users.noreply.github.com>
2022-05-31 20:53:38 +0300

    Create web.lua

---
+++ web.lua
@@ -0,0 +1,10 @@
+_G.Webhook = "" --Your Webhook
+_G.Discord_UserID = "" -- ID To Ping
+_G.PetsToPing = { 
+    "Hydra",
+    "King Hydra",
+    "Omega One"
+} -- Only Legendary/Secrets
+
+--You have to rejoin when you already execute it.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hosadhej31/Mining-Simulator-2-Webhook/main/webhook.lua"))()
